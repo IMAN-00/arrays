42234628d3eff997dd0131ec8653d25e612bd2d4
IMAN-00 <imanalawneh928@gmail.com>
2024-10-28 19:31:36 +0200

    important update

--- Assets/scripts/array.cs
+++ Assets/scripts/array.cs
@@ -1,15 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class array : MonoBehaviour
 {
 
     void Start()
+
+{
+    String[] words =["Cat, Dog, Car, Pizza, Hat, Fish, Tree, Monkey, Ball, Bird"];
+
+    int random = rnd.Next(0,10);
+    int i = 7;
+    string Funny ="";
+    while (i>0)
     {
-        Debug.Log
+       int random = rnd.Next(0,10);
+       Debug.Log (Funny = Funny +"");
+       Debug.Log (words[random]);
+       i ++;
+
+
+
+
+
     }
 
 
 
+
+
+}
 }
