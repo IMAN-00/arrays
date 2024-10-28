eec708372a9ffb4bca274606c82b03527dbafcaa
IMAN-00 <imanalawneh928@gmail.com>
2024-10-28 19:34:39 +0200

    another update

--- Assets/scripts/array.cs
+++ Assets/scripts/array.cs
@@ -15,20 +15,16 @@ public class array : MonoBehaviour
     int random = rnd.Next(0,10);
     int i = 7;
     string Funny ="";
-    while (i>0)
+    while (i>=0)
     {
        int random = rnd.Next(0,10);
        Debug.Log (Funny = Funny +"");
        Debug.Log (words[random]);
        i ++;
 
-
-
-
-
     }
 
-
+     Debug.Log ("Funny sentence");
 
 
 
